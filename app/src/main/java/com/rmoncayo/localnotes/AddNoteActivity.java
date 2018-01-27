bf19bfa56d213babbbb6a33c610c04415637d01f
raul m <moncayoboy12@gmail.com>
2018-01-27 13:09:11 -0800

    Finishing recording button. Saves to internal storage as .3gp

--- app/src/main/java/com/rmoncayo/localnotes/AddNoteActivity.java
+++ app/src/main/java/com/rmoncayo/localnotes/AddNoteActivity.java
@@ -1,13 +1,120 @@
 package com.rmoncayo.localnotes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+
+import static java.text.DateFormat.getDateTimeInstance;
+
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String mFileName = null;
+
+    private RecordButton mRecordButton = null;
+    private MediaRecorder mRecorder = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted) finish();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        mFileName = getExternalCacheDir().getAbsolutePath();
+        DateFormat timeStampFormat = getDateTimeInstance();
+        Date myDate = new Date();
+        mFileName += "/" + timeStampFormat.format(myDate)+".3gp";
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        mRecordButton = new RecordButton(this);
+        LinearLayout recordButtonHolder = findViewById(R.id.record_audio_button_holder);
+        recordButtonHolder.addView(mRecordButton);
+
+
+
+    }
+
+    public class RecordButton extends AppCompatButton {
+        boolean mStartRecording = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    setText(R.string.stop_recording_button_string);
+                } else {
+                    setText(R.string.start_recording_button_string);
+                }
+                mStartRecording = !mStartRecording;
+            }
+        };
+
+        public RecordButton(Context ctx) {
+            super(ctx);
+            setText(R.string.start_recording_button_string);
+            setOnClickListener(clicker);
+        }
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    //TODO 1/27 think about handling recording and then losing focus/stopping
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        //TODO 1/27 maybe add ability to change audio recording quality in a settings menu
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        Log.d(LOG_TAG, mFileName);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.reset();
+        mRecorder.release();
+        mRecorder = null;
     }
 }
